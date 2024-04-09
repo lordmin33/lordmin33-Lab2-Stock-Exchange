d2432dadf0dd919e01d35aa60f229fe4e27c7ac5
lordmin33 <34090637+lordmin33@users.noreply.github.com>
2024-04-09 11:43:44 +0200

    Add files via upload

---
+++ Lab2 Haskell/Lab2.hs
@@ -0,0 +1,66 @@
+import Control.Applicative
+import System.Environment
+import System.IO
+
+-- | Bids.
+
+data Bid
+  = Buy Person Price           -- Person offers to buy share
+  | Sell Person Price          -- Person offers to sell share
+  | NewBuy Person Price Price  -- Person changes buy bid
+  | NewSell Person Price Price -- Person changes sell bid
+  deriving Show
+
+type Person = String
+type Price = Integer
+
+-- | Parses a bid. Incorrectly formatted bids are returned verbatim
+-- (tagged with 'Left').
+
+parseBid :: String -> Either String Bid
+parseBid s = case words s of
+  name : kind : prices ->
+    case (kind, mapM readInteger prices) of
+      ("K",  Just [price])              -> Right (Buy name price)
+      ("S",  Just [price])              -> Right (Sell name price)
+      ("NK", Just [oldPrice, newPrice]) -> Right (NewBuy name oldPrice newPrice)
+      ("NS", Just [oldPrice, newPrice]) -> Right (NewSell name oldPrice newPrice)
+      _ -> Left s
+  _ -> Left s
+  where
+  readInteger :: String -> Maybe Integer
+  readInteger s = case filter (null . snd) $ reads s of
+    [(x, _)] -> Just x
+    _        -> Nothing
+
+-- | Parses a sequence of bids. Correctly formatted bids are returned
+-- (in the order encountered), and an error message is printed for
+-- each incorrectly formatted bid.
+
+parseBids :: String -> IO [Bid]
+parseBids s = concat <$> mapM (check . parseBid) (lines s)
+  where
+  check (Left bid)  = do
+    hPutStrLn stderr $ "Malformed bid: " ++ bid
+    return []
+  check (Right bid) = return [bid]
+
+-- | The main function of the program.
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    []  -> process stdin
+    [f] -> process =<< openFile f ReadMode
+    _   -> hPutStr stderr $ unlines
+      [ "Usage: ./Lab2 [<file>]"
+      , "If no file is given, then input is read from standard input."
+      ]
+  where
+  process h = trade =<< parseBids =<< hGetContents h
+
+-- | The core of the program. Takes a list of bids and executes them.
+
+trade :: [Bid] -> IO ()
+trade = undefined
