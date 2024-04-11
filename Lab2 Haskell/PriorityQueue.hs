7d67c2671f8dac0f16b6ab8e2dd17d97f57bc757
lordmin33 <albin.karlsteen@hotmail.com>
2024-04-11 10:45:47 +0200

    si

--- Lab2 Haskell/PriorityQueue.hs
+++ Lab2 Haskell/PriorityQueue.hs
@@ -1,18 +1,18 @@
 module Lab2 where
 
 data SkewHeap a = Empty
-                | Node a (SkewHeap a) (SkewHeap a)
+                | Node a (SkewHeap a) (SkewHeap a) deriving (Show)
 
 ivariant :: Ord a => a -> SkewHeap a
 ivariant a = a
 
-merge :: Ord a ->  SkewHeap a -> SkewHeap a -> SkewHeap a
+merge :: Ord a =>  SkewHeap a -> SkewHeap a -> SkewHeap a
 merge sh1 sh2 = undefined
 
-insert :: Ord a -> a -> SkewHeap a -> SkewHeap a 
+insert :: Ord a => a -> SkewHeap a -> SkewHeap a 
 insert a sh = undefined
 
-extracte_min :: Ord a -> SkewHeap a -> SkewHeap a 
+extracte_min :: Ord a => SkewHeap a -> SkewHeap a 
 extracte_min sh = undefined
 
 
