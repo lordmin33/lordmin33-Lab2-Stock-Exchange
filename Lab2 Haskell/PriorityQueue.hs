922fab45e36281637e84f4d4420845c50ea97d27
lordmin33 <albin.karlsteen@hotmail.com>
2024-04-11 10:58:00 +0200

    te

--- Lab2 Haskell/PriorityQueue.hs
+++ Lab2 Haskell/PriorityQueue.hs
@@ -3,8 +3,9 @@ module Lab2 where
 data SkewHeap a = Empty
                 | Node a (SkewHeap a) (SkewHeap a) deriving (Show)
 
+-- should make sure value can't be changed
 ivariant :: Ord a => a -> SkewHeap a
-ivariant a = a
+ivariant a = 
 
 merge :: Ord a =>  SkewHeap a -> SkewHeap a -> SkewHeap a
 merge sh1 sh2 = undefined
@@ -16,5 +17,7 @@ extracte_min :: Ord a => SkewHeap a -> SkewHeap a
 extracte_min sh = undefined
 
 
-delete :: Ord a -> SkewHeap a -> SkewHeap a 
+delete :: Ord a => SkewHeap a -> SkewHeap a 
 delete sh = undefined
+
+
