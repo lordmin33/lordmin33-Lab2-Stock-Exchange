36d1adefc51a43e80e744c7f53a1b7ae24658f65
lordmin33 <albin.karlsteen@hotmail.com>
2024-04-11 10:19:02 +0200

    undefined

--- Lab2 Haskell/priorityQueue.hs
+++ Lab2 Haskell/priorityQueue.hs
@@ -2,7 +2,15 @@ data SkewHeap a = Empty
                 | Node a (SkewHeap a) (SkewHeap a)
 
 
+merge :: Ord a ->  SkewHeap a -> SkewHeap a -> SkewHeap a
+merge = undefined
 
+add_value :: Ord a -> a -> SkewHeap a -> SkewHeap a 
+add_value = undefined
+
+delete_min :: Ord a -> SkewHeap a -> SkewHeap a 
+delete_min = undefined
 
 
 delete :: Ord a -> SkewHeap a -> SkewHeap a 
+delete = undefined
