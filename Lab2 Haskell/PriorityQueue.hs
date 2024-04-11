077318e4fedefbd8e5ddd89bad03d4bf1e3c73ea
Marabou03 <153927798+Marabou03@users.noreply.github.com>
2024-04-11 10:50:47 +0200

    first try pt.1

--- Lab2 Haskell/PriorityQueue.hs
+++ Lab2 Haskell/PriorityQueue.hs
@@ -6,6 +6,9 @@ data SkewHeap a = Empty
 ivariant :: Ord a => a -> SkewHeap a
 ivariant a = a
 
+singleton :: Ord a => a -> SkewHeap a
+singleton x = node x Empty Empty
+
 merge :: Ord a =>  SkewHeap a -> SkewHeap a -> SkewHeap a
 merge sh1 sh2 = undefined
 
